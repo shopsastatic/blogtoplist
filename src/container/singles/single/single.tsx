a4a2acfc4fd5a027ff4701556e7dbf8d481195dc
Khai Dang <akhai7230@gmail.com>
2024-07-10 12:37:27 +0700

    Update

--- src/container/singles/single/single.tsx
+++ src/container/singles/single/single.tsx
@@ -149,7 +149,7 @@ const SingleType1: FC<SingleType1Props> = ({ post, showRightSidebar }) => {
           </div>
         )}
 
-        {footer_category.slice(0, 4).length > 0 || footer_category.slice(4, 8).length > 0 || footer_category.slice(8, 12).length > 0 && (
+        {footer_category.slice(0, 4).length > 0 && (
           <div className="footer-category container mt-28">
             <Link href={categoriesTyped?.nodes?.[0]?.uri ?? "/"} className="block w-fit my-14">
               <h3>{categoriesTyped?.nodes?.[0]?.name ?? ""}</h3>
